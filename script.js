60465ec789beb6ab32507f261464a5382eb11859
Lina <lina.art777@icloud.com>
2021-06-06 15:39:12 +0300

    js question

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 let hello;
 hello='привет';
 console.log(hello);
+ let question1 = {
+   question: 'name',
+   answer: Jone,
+   currectAnswer: Joy
+ };
+ let question2 = {
+   question: 'surnname',
+   answer: Smit,
+   currectAnswer: Smat
+ };
+ let question3 = {
+   question: 'age',
+   answer: 30,
+   currectAnswer: 21
+ };
+ let question4 = {
+   question: 'mail',
+   answer: 5,
+   currectAnswer: 4
+ };
